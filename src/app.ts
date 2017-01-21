b9fda1814294a813ad325a04520b523b386d9f3a
Artiom FEDOROV <fedorov.artiom@gmail.com>
2017-01-21 02:09:43 +0100

    clean current workspace

--- src/app.ts
+++ src/app.ts
@@ -1,28 +1,3 @@
-import {Configuration} from './configurations/ConfigurationLoader';
-import {JenkinsClientApi} from './client_api/JenkinsClientApi';
-import {UsbInfraRedDevice} from './physical_device/UsbInfraRedDevice';
-
+import {Configuration} from './configurations/ConfigurationLoader'
 let config:any = Configuration
-let jenkins_client_api = new JenkinsClientApi( config.jenkins_url )
-let ir_device = new UsbInfraRedDevice()
-ir_device.setDevicePath( config.usb_infrared_device_path )
-
 console.log("App ready, main loop starts")
-setInterval(() => {
-    jenkins_client_api.getJobInfo(config.jenkins_job_name).then((data: any) => {
-        console.log(data.color)
-        let jenkins_color_name = 'jenkins_color_' + data.color
-        // check jenkins color has ir message definition
-        if (config[jenkins_color_name] !== undefined) {
-            ir_device.sendIrMessage(config[jenkins_color_name])
-        } else {
-            console.log("Jenkins color value : " + jenkins_color_name + " has no ir message defined")
-        }
-    })
-    .catch((err) => {
-        console.log(err)
-        if (err === true) {
-            console.log("Job name not found")
-        }
-    })
-}, config.refresh_timeout_seconds * 1000);
